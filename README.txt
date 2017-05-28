1e11deb9470d29677888a65fd177f9c2bc50b959
Horacio Galindo <horaciodev@gmail.com>
2017-05-28 13:17:09 -0700

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+AspNet Core MVC Web application secured by Identity Server 4 - Implicit Flow
+1.Clone Repo
+2.Run >dotnet restore
+3.Run >dotnet build
+You should be running on port http://localhost:5002
+
+Depends on having an Identity Server 4 properly configured and running @ http://localhost:5000
+Identity server is in the following repository: https://github.com/horaciodev/IdSvr4POC   (dev branch)
---
+++ Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+using System.IdentityModel.Tokens.Jwt;
+
+using IdentityModel;
+using IdentityServer4.Stores;
+
+namespace IdSvr4.MVCNetCoreClient
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            #region beforeMVC
+            //the following two should always go before MVC in the pipeline
+            app.UseCookieAuthentication(new CookieAuthenticationOptions{
+                AuthenticationScheme = "Cookies", //Cookies or cookie?
+                AutomaticAuthenticate = true,
+                ExpireTimeSpan = TimeSpan.FromMinutes(60)
+            });
+
+            
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions{
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+                Authority = "http://localhost:5000",
+                RequireHttpsMetadata = false,
+                ClientId = "CoreMvcApp",
+                //ClientSecret = "secret",
+                ResponseType = "id_token", //"code id_token"
+                Scope = {"openid","profile","email"},
+                GetClaimsFromUserInfoEndpoint = true,
+                SaveTokens = true,
+
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = JwtClaimTypes.Name,
+                    RoleClaimType = JwtClaimTypes.Role
+                },
+            });
+
+            #endregion
+
+             app.UseMvcWithDefaultRoute();
+        }
+    }
+}
